d0017da1c64248384ef8c0a094d1e7682adb6c5f
Vishal Chauhan <148201865+vishalchauhan2109@users.noreply.github.com>
2025-01-14 13:54:41 +0530

    coin combination to complete an amount

---
+++ code (2).java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class main{
+     
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int []arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            
+            arr[i] = scn.nextInt();
+        }
+        
+        int target = scn.nextInt();
+        
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        
+        for(int i = 0; i < arr.length; i++){
+            for(int j = arr[i]; j < dp.length; j++){
+                dp[j] += dp[j - arr[i]];
+                
+            }
+        }
+        
+        System.out.println(dp[target]);
+        
+    } 
+}
