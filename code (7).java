7912493cf097f65a496ed31384bb07a4d33a3fd5
Vishal Chauhan <148201865+vishalchauhan2109@users.noreply.github.com>
2025-01-31 12:02:19 +0530

    Maximum Sum Non Adjacent Elements Dynamic Programming | Greedy Approach
    Maximum Sum Non Adjacent Elements Dynamic Programming | Greedy Approach

---
+++ code (7).java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class main{
+     // Maximum Sum Non Adjacent Elements Dynamic Programming | Greedy Approach
+     
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        int inc = arr[0];
+        int exc = 0;
+        
+        for(int i = 1; i < arr.length; i++){
+            int ninc = exc + arr[i];
+            int nexc = Math.max(inc,exc);
+            
+            inc = ninc;
+            exc = nexc;
+        }
+        int ans = Math.max(inc,exc);
+        System.out.println(ans);
+        
+        
+      
+    } 
+}
