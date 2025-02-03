502dcc919d06715281cd1c7afc80a1d1a6c3bf58
Vishal Chauhan <148201865+vishalchauhan2109@users.noreply.github.com>
2025-02-03 12:40:23 +0530

    Friends Pairing Problem Dynamic Programming
    Friends Pairing Problem Dynamic Programming

---
+++ code (14).java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class Main {
+    //Friends Pairing Problem Dynamic Programming 
+    
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      
+      int [] dp = new int[n+1];
+      dp[1] = 1;
+      dp[2] = 2;
+      
+      
+      
+      for(int i = 3; i <= n; i++){
+          dp[i] = dp[i-1] + dp[i-2]*(i-1);
+      }
+      
+      System.out.println(dp[n]);
+     
+      
+  }
+  
+}
